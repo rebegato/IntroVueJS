3908a10750260e127ed885b9d3a33f5b25b6812a
rebegato <rebegato11@gmail.com>
2020-06-26 08:19:42 +0000

    Example 2

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        image: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks"
     }
 })
