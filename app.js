145dff8f5ea2526c60c5a988b91d96228679bce4
rebegato <rebegato11@gmail.com>
2020-06-26 15:02:59 +0000

    Example 4 - List Rendering

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ var app = new Vue({
         href: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: false,
         inventory: -1,
-        onSale: true
+        onSale: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "Green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "Blue"
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
      }
 })
