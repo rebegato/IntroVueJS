fedcc41793585f42ae561d709c42ccf65463855e
rebegato <rebegato11@gmail.com>
2020-06-28 02:43:49 +0000

    Example 7 Computed Properties

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# vue-exProy1
+# Vue-exProy1
 Vue Example Project 1.
+
+# What else should we know?
+
+Computed properties are cached, meaning the result is saved until its dependencies change. So when quantity changes, the cache will be cleared and the **next time you access the value of inStock , it will return a fresh result, and cache that result.
+
+With that in mind, it’s more efficient to use a computed property rather than a method for an expensive operation that you don’t want to re-run every time you access it.
+
+It is also important to remember that you should not be mutating your data model from within a computed property. You are merely computing values based on other values. Keep these functions pure.
--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: "Vue Mastery",
         product: "Socks",
-        image: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         href: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: false,
-        inventory: -1,
-        onSale: true,
+        selectVariant:0,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg"
+                variantQuantity: 10,
+                variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+                onSale: true
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg"
+                variantQuantity: 0,
+                variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
+                onSale: false
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -31,8 +33,22 @@ var app = new Vue({
             removeFromCart: function() {
                 this.cart -= 1;
             },
-            updateProduct(variantImage) {
-                this.image = variantImage;
+            updateProduct(index) {
+                this.selectVariant = index;
+            }
+        },
+        computed:{
+            title() {
+                return this.brand + " " + this.product;
+            },
+            image() {
+                return this.variants[this.selectVariant].variantImage;
+            },
+            inStock(){
+                return this.variants[this.selectVariant].variantQuantity;
+            },
+            onSale() {
+                return (this.variants[this.selectVariant].onSale)? "On Sale!":""; 
             }
         }
 })
