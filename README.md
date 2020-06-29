cb147567198c50ec50393daf7ce779ce4629b57a
rebegato <rebegato11@gmail.com>
2020-06-29 02:02:45 +0000

    Example 8 Components

--- README.md
+++ README.md
@@ -18,3 +18,14 @@ Computed properties are cached, meaning the result is saved until its dependenci
 With that in mind, it’s more efficient to use a computed property rather than a method for an expensive operation that you don’t want to re-run every time you access it.
 
 It is also important to remember that you should not be mutating your data model from within a computed property. You are merely computing values based on other values. Keep these functions pure.
+
+## Components
+
+* Components are blocks of code, grouped together within a custom element
+* Components make applications more manageable by breaking up the whole into reusuable parts that have their own structure and behavior
+* Data on a component must be a function
+* Props are used to pass data from parent to child
+* We can specify requirements for the props a component is receiving
+* Props are fed into a component through a custom attribute
+* Props can be dynamically bound to the parent’s data
+* Vue dev tools provide helpful insight about your components
--- app.js
+++ app.js
@@ -1,54 +1,131 @@
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <div class="product-details">
+            <h2> Product Details </h2>
+            <ul>
+                <li v-for="detail in details"> <p>{{ detail }}</p> </li>
+            </ul>
+        </div>
+    `
+})
+
+Vue.component('product', {
+    props:{
+        premium: {
+            type: Boolean,
+            required: true 
+        }
+    },
+    template : `
+            <div class="product">
+
+            <div class="product-image">
+                <img v-bind:src="image" v-bind:alt="altText" />
+                <div class="cart">
+                    <p> Cart({{cart}}) </p>
+                </div>                
+                <button :class="{ disabledButton: !inStock }" 
+                    v-on:click="addToCart" :disabled="!inStock">Add to Cart</button>
+                <button @click="removeFromCart">Remove from Cart</button>
+            </div>
+
+            <div class="product-info">
+                <h1> {{ title }} </h1>
+                <a :href="href" target="_blank">More products like this...</a>
+                <br/>
+                <p v-if="inStock > 10">In Stock</p>
+                <p v-else-if="inStock <=10 && inStock > 0">Almost sold out!</p>
+                <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
+                <span v-show="onSale">On Sale!</span>
+                <p> Shipping: {{ shipping }} </p>
+
+                <product-details :details="details"></product-details>
+
+                <h2> Colors </h2>
+                <div class="color-box" v-for="(variant, index) in variants" 
+                       :key="variant.variantId"
+                       :style="{ backgroundColor: variant.variantColor }" 
+                       @mouseover="updateProduct(index)">
+                </div>
+
+                <h2> Sizes </h2>
+                <ul>
+                    <li v-for="size in sizes">{{ size }}</li>
+                </ul>
+
+            </div>
+
+        </div>
+    `,
+    data() {
+        return {
+            brand: "Vue Mastery",
+            product: "Socks",
+            altText: "A pair of socks",
+            href: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+            selectVariant: 0,
+            details: ["80% cotton", "20% polyester", "Gender-neutral"],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "green",
+                    variantQuantity: 10,
+                    variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+                    onSale: true
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "blue",
+                    variantQuantity: 0,
+                    variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
+                    onSale: false
+                }
+            ],
+            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1;
+        },
+        removeFromCart: function () {
+            this.cart -= 1;
+        },
+        updateProduct(index) {
+            this.selectVariant = index;
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product;
+        },
+        image() {
+            return this.variants[this.selectVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectVariant].variantQuantity;
+        },
+        onSale() {
+            return (this.variants[this.selectVariant].onSale) ? "On Sale!" : "";
+        },
+        shipping() {
+            if(this.premium) {
+                return "Free";
+            } return 2.99;
+        }
+    }
+})
+
 var app = new Vue({
     el: '#app',
     data: {
-        brand: "Vue Mastery",
-        product: "Socks",
-        altText: "A pair of socks",
-        href: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        selectVariant:0,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "green",
-                variantQuantity: 10,
-                variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
-                onSale: true
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue",
-                variantQuantity: 0,
-                variantImage: "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
-                onSale: false
-            }
-        ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        cart: 0
-        },
-        methods: {
-            addToCart: function () {
-                this.cart += 1;
-            },
-            removeFromCart: function() {
-                this.cart -= 1;
-            },
-            updateProduct(index) {
-                this.selectVariant = index;
-            }
-        },
-        computed:{
-            title() {
-                return this.brand + " " + this.product;
-            },
-            image() {
-                return this.variants[this.selectVariant].variantImage;
-            },
-            inStock(){
-                return this.variants[this.selectVariant].variantQuantity;
-            },
-            onSale() {
-                return (this.variants[this.selectVariant].onSale)? "On Sale!":""; 
-            }
-        }
+        premium: false
+    }
 })
